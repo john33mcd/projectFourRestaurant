101743643553675f1763abcfd8becc50ed239a56
john33mcd <jmcd-34@hotmail.com>
2023-04-21 12:32:22 +0000

    added sample reservation model to admin panel

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Reservation(models.Model):
+    Name = models.CharField(max_length=50, null=False, blank=False)
+    date = models.DateField(auto_now_add=True, null=False, blank=False)
+    time = models.TimeField(auto_now=False, null=False, blank=False)
