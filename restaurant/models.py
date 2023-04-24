f1b9ea1033044b64eacdb8b4316f7ad62c09d8e0
john33mcd <jmcd-34@hotmail.com>
2023-04-24 17:29:23 +0000

    added model for date and time

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
 
 
 class Reservation(models.Model):
-    Name = models.CharField(max_length=50, null=False, blank=False)
-    date = models.DateField(auto_now_add=True, null=False, blank=False)
-    time = models.TimeField(auto_now=False, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    date = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.name
